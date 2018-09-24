fa3cbf2d99dcf3a10fb682d2e2824b28efb455eb
clifford <clifford@noukod.com>
2018-09-24 05:24:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,18 +22,22 @@ end
 
 def long_planeteer_calls# code an argument here
   # Your code here
-#def long_planeteer_calls(array) 
-  i = 0 
+#def long_planeteer_calls(array)
+  i = 0
   if  array.any? {|i| i.length > 4}
     return true
-  else 
+  else
     return false
   i = i + 1
-  end 
+  end
 #end
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  while i < cheese_types.length
+    return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1
+  end
 end
